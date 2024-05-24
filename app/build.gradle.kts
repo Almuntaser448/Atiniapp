7f1114cc3fa8e94bbdbd65200763904991315fe3
Almuntaser448 <77934756+Almuntaser448@users.noreply.github.com>
2024-05-24 16:16:24 +0200

    first version errors free

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,63 +1,35 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.android")
 }
 
-val buildToolsVersion = "30.0.3"
-val minSdkVersion = 21
-val compileSdkVersion = 30
-val targetSdkVersion = 30
-val kotlinVersion = "1.5.10"
-
 android {
-    compileSdkVersion(compileSdkVersion)
-    buildToolsVersion(buildToolsVersion)
-
+    namespace ="com.rassam.atiniapp"
+    compileSdkVersion(33)
     defaultConfig {
-        applicationId = "com.example.atiniapp"
-        minSdkVersion(minSdkVersion)
-        targetSdkVersion(targetSdkVersion)
+        applicationId = "com.rassam.atiniapp"
+        minSdkVersion(21)
+        targetSdkVersion(33)
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.recyclerview:recyclerview:1.2.0")
-
-    // AWS SDK dependencies
-    implementation("com.amazonaws:aws-android-sdk-core:2.22.5")
-    implementation("com.amazonaws:aws-android-sdk-cognitoidentityprovider:2.22.5")
-    implementation("com.amazonaws:aws-android-sdk-s3:2.22.5")
-    implementation("com.amazonaws:aws-android-sdk-dynamodb:2.22.5")
-
-    // Glide for image loading
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    kapt("com.github.bumptech.glide:compiler:4.12.0")
-
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation(libs.firebase.common)
+    implementation(libs.firebase.firestore)
+    implementation("com.google.android.material:material:1.5.0")
+    implementation(libs.firebase.storage) // Updated Kotlin version
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    val kotlin_version = "1.8.0"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath("com.android.tools.build:gradle:8.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
@@ -19,6 +17,3 @@ allprojects {
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
